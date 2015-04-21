5008219882374f16c2b3e4ba459d736c0888cdbe
Christian Vogel <vogelchr@vogel.cx>
2015-04-21 09:22:02 +0200

    update_perms: create gpio sysfs directory

--- update_perms.sh
+++ update_perms.sh
@@ -2,6 +2,9 @@
 set -x -v
 
 chmod 666 /dev/spidev0*
+if ! [ -d /sys/class/gpio/gpio25 ]  ; then
+	echo 25 >/sys/class/gpio/export
+fi
 chmod 666 /sys/class/gpio/gpio25/direction
 chmod 666 /sys/class/gpio/gpio25/value
 
